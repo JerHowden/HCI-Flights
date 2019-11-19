94d919b5e198e019875473482dc742105bb96a76
Elias Moreno <elias.cmoreno17@gmail.com>
2019-11-19 14:24:32 -0600

    fixing mapbox

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-import ReactMapGL, { NavigationControl} from 'react-map-gl'
-
-const navStyle = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    padding: '10px'
-};
+import MapGL from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default class Map extends Component {
     constructor(props) {
@@ -14,37 +8,23 @@ export default class Map extends Component {
 
         this.state = {
 
-            viewport: {
-                latitude: 37.785164,
-                longitude: -100,
-                zoom: 2.8,
-                bearing: 0,
-                pitch: 0,
-                width: 500,
-                height: 500,
-            },
-
             apiKey: 'pk.eyJ1IjoiZWxpYXNjbTE3IiwiYSI6ImNrMzR4NmJvdzFhOW8zbXBweXUwcHIwdDYifQ.T_3ZZklfpxf5b8wibfI0ew'
+
         }
     }
 
-
-
     render() {
-
-        const {viewport} = this.state.viewport
-
         return (
-            <div id="Map">
-                <ReactMapGL
-                {...viewport}
-                mapStyle="mapbox://styles/mapbox/dark-v9"
-                mapboxApiAccessToken={this.state.apiKey}>
-                <div className="nav" style={navStyle}>
-                    <NavigationControl/>
-                </div>    
-                </ReactMapGL>
-            </div>
+            <div class='map'>
+                <MapGL 
+                    style={{ width: '100%', height: '90%', overflow: 'visible'}}
+                    mapStyle="mapbox://styles/mapbox/dark-v9"
+                    mapboxApiAccessToken={this.state.apiKey}
+                    latitude={37.78}
+                    longitude={-122.41}
+                    zoom={11}
+                />
+            </div >
         )
     }
 }
