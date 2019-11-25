02f3498574d2828a4d97381130ae5112067bbe67
Eliascm17 <Elias.cmoreno17@gmail.com>
2019-11-25 12:26:41 -0600

    Hover isn't quite working and idk why :/

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import ReactMapGL, {Source, Marker, Layer} from 'react-map-gl'
-// import WebMercatorViewport from 'viewport-mercator-project';
+import ReactMapGL from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
-import buildings from "./Buildings.geojson"
-
+import buildingsData from "./Buildings.geojson"
 import Panel from './Panel'
+
 
 export default class Map extends Component {
     constructor(props) {
@@ -17,7 +16,7 @@ export default class Map extends Component {
                 height: window.innerHeight,
                 latitude: 33.594,
                 longitude: -101.891,
-                zoom: 14
+                zoom: 14,
             },
 
             apiKey: 'pk.eyJ1IjoiZWxpYXNjbTE3IiwiYSI6ImNrMzR4NmJvdzFhOW8zbXBweXUwcHIwdDYifQ.T_3ZZklfpxf5b8wibfI0ew'
@@ -25,7 +24,6 @@ export default class Map extends Component {
         }
     }
 
-    // add color to the buildings on component will mount lifecycle??
     // change lat, long, and zoom on click or search
     // change the color of TTU related buildings on hover
     // display left justified panel on click or search
@@ -34,13 +32,16 @@ export default class Map extends Component {
 
 componentDidMount(){
 
+        //create map object
         const map = this.reactMap.getMap();
-        console.log('map', map);
-        //load geojson
-        const TTU = buildings
-        
+        // console.log('map', map);
+
+        //load geojson data
+        const TTU = buildingsData;
+        var hoveredStateId = null;
+
         map.on('load', () => {
-            //do shit here with source and layer lmao
+
             map.addSource("Building Data", {
                 "type": "geojson",
                 "data": TTU
@@ -48,7 +49,7 @@ componentDidMount(){
 
             //fills for the building polygons
             map.addLayer({
-                "id": "buildings-fills",
+                "id": "building-fills",
                 "type": "fill",
                 "source": "Building Data",
                 "layout": {
@@ -56,17 +57,16 @@ componentDidMount(){
                     // "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"]
                 },
                 "paint": {
-                    "fill-color": "#c6eb34",
-                    "fill-opacity": 0.8
-                    //["cfase",
-                    //     ["boolean", ["feature-state", "hover"], false],
-                    //     1,
-                    //     0.5
-                    //]
+                    "fill-color": "#627BC1",
+                    "fill-opacity": ["case",
+                        ["boolean", ["feature-state", "hover"], false],
+                        1,
+                        0.2
+                    ]
                 }
             });
             
-            //borders for the building polygons
+            // borders for the building polygons
             map.addLayer({
                 "id": "building-borders",
                 "type": "line",
@@ -78,53 +78,44 @@ componentDidMount(){
                 }
             });
 
-        });
-
-    
-
-        var hoveredStateId = null;
-
-        //on hover fill in the polygon
-        map.on("mousemove", "building-fills", function (e) {
-            if (e.features.length > 0) {
-                if (hoveredStateId) {
-                    map.setFeatureState({ source: 'buildings', id: hoveredStateId }, { hover: false });
+            map.on('mousemove', (e) => {
+                console.log(e.lngLat)
+            });
+        
+             map.on("mousemove", "building-fills", function (e) {
+                console.log(e)
+                if (e.features.length > 0) {
+                    if (hoveredStateId) {
+                        map.setFeatureState({ source: 'Building Data', id: hoveredStateId }, { hover: false });
+                    }
+                    hoveredStateId = e.features[0].id;
+                    map.setFeatureState({ source: 'Building Data', id: hoveredStateId }, { hover: true });
                 }
-                hoveredStateId = e.features[0];
-                map.setFeatureState({ source: 'buildings', id: hoveredStateId }, { hover: true });
-            }
-        });
+            });
 
-        // //on mouse leave, put the polygon back to its original state
-        map.on("mouseleave", "building-fills", function () {
-            if (hoveredStateId) {
-                map.setFeatureState({ source: 'buildings', id: hoveredStateId }, { hover: false });
-            }
-            hoveredStateId = null;
+
+            map.on("mouseleave", "building-fills", function () {
+                if (hoveredStateId) {
+                    map.setFeatureState({ source: 'Building Data', id: hoveredStateId }, { hover: false });
+                }
+                hoveredStateId = null;
+            });
+
         });
         
-        // let viewport = {
-        //     width: window.innerWidth,
-        //     height: window.innerHeight,
-        //     latitude: 33.594,
-        //     longitude: -101.891,
-        //     zoom: 14
-        // }
-        // this.setState({ viewport }, () => console.log("Viewport", viewport, this.state))
-
     }
 
 
     render() {
         return (
             <div>
-                <input 
+                {/* <input 
                     id='pac-input'
                     className='controls'
                     type='text'
                     placeholder='Search TTU'
                      // Need to figure out how to create an autocomplete feature for the local json data
-                />
+                /> */}
                 <ReactMapGL 
                     ref={(reactMap) => this.reactMap = reactMap}
                     style={{ width: '100%', height: '100%'}}
