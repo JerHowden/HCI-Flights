bb34cc215a26185ffd4fa099a8fced42438f7fd7
Jeremiah Howden <jeremiahhowden@gmail.com>
2019-12-02 00:06:47 -0600

    Updates

---
+++ precache-manifest.80fa790865146ad136324e73b28d21ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616bb2c9c1b619cc662b303f23e74c0e",
+    "url": "/TexasTechFreshmanGuide/index.html"
+  },
+  {
+    "revision": "7d831085579243ec924b",
+    "url": "/TexasTechFreshmanGuide/static/css/2.d9a8a5a7.chunk.css"
+  },
+  {
+    "revision": "74b6c59785db89902cc9",
+    "url": "/TexasTechFreshmanGuide/static/css/main.5599a9ab.chunk.css"
+  },
+  {
+    "revision": "7d831085579243ec924b",
+    "url": "/TexasTechFreshmanGuide/static/js/2.389c2a82.chunk.js"
+  },
+  {
+    "revision": "74b6c59785db89902cc9",
+    "url": "/TexasTechFreshmanGuide/static/js/main.e0fc2d7b.chunk.js"
+  },
+  {
+    "revision": "9ba37162a2118e2ef332",
+    "url": "/TexasTechFreshmanGuide/static/js/runtime-main.99b38b94.js"
+  },
+  {
+    "revision": "ad9d51cfd0ef3ca69c59f85ec9cfaf2c",
+    "url": "/TexasTechFreshmanGuide/static/media/Buildings.ad9d51cf.geojson"
+  }
+]);
