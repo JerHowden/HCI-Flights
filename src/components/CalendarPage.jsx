c83aa5dfbc15c9d07394d50fdadaa5883b3ec554
Jeremiah Howden <jeremiahhowden@gmail.com>
2019-12-08 19:23:38 -0600

    Final Changes

--- src/components/CalendarPage.jsx
+++ src/components/CalendarPage.jsx
@@ -6,7 +6,7 @@ import moment from 'moment'
 import { Grid } from '@material-ui/core'
 
 import Events from '../data/Events.json'
-import SportsEvents from '../data/AthleticEvents.json'
+import AthleticEvents from '../data/AthleticEvents.json'
 import AcademicCalendar from '../data/AcademicCalendar.json'
 import './CalendarPage.css'
 
@@ -54,15 +54,53 @@ export default class CalendarPage extends Component {
 				<Calendar
 					ref={calendar => this.calendar = calendar}
 					localizer={localizer}
-					events={AcademicCalendar.vcalendar[0].vevent.map(event => {
-						return({
-							title: event.summary,
-							start: moment.utc(event.dtstart[0]).toDate(),
-							end: moment.utc(event.dtend[0]).toDate(),
-							allDay: !!event.dtstart[1].value,
-							category: "Academic",
-						})
-					})}
+					drilldownView="agenda"
+					views={['month', 'agenda']}
+					length={0}
+					eventPropGetter={event => ({className: 'category-' + event.category.toLowerCase()})}
+					events={
+						AcademicCalendar.vcalendar[0].vevent.map(event => {
+							return({
+								title: event.summary,
+								start: moment(event.dtstart[0]).toDate(),
+								end: moment(event.dtend[0]).toDate(),
+								allDay: !!event.dtstart[1].value,
+								category: "Academic",
+							})
+						}).concat(AthleticEvents.map(event => {
+							let start = moment(event["Start Date"])
+							let end = moment(event["End Date"])
+							let allDay = !event["Start Time"] || event["Start Time"].replace(/ /g,'') === "AllDay"
+							if(!allDay) {
+								let startSplit = event["Start Time"].split(" ")[0].split(":")
+								start.hour(startSplit[0] || 0)
+								start.minute(startSplit[1] || 0)
+								let endSplit = event["End Time"].split(" ")[0].split(":")
+								end.hour(endSplit[0] || 0)
+								end.minute(endSplit[1] || 0)
+								if(event["Start Time"].includes("P") || event["Start Time"].includes("p")) 
+									start.hour(startSplit[0] + 12 || 0)
+								if(event["End Time"].includes("P") || event["End Time"].includes("p")) 
+									end.hour(endSplit[0] + 12 || 0)
+							}
+							return({
+								title: event.Event,
+								start: start.toDate(),
+								end: end.toDate(),
+								allDay: allDay,
+								category: "Athletic",
+							})
+						})).concat(Events.bwEventList.events.map(event => {
+							console.log(event.start.dateTime, event.end.dateTime)
+							return {
+								title: event.summary,
+								start: moment(event.start.datetime, ["YYYYMMDDTHHmmss", "YYYYMMDD"]).toDate(),
+								end: moment(event.end.datetime, ["YYYYMMDDTHHmmss", "YYYYMMDD"]).toDate(),
+								allDay: event.start.allDay,
+								category: "Events",
+							}
+						}))
+					}
 					startAccessor="start"
 					endAccessor="end"
 					style={{height: 500}}
